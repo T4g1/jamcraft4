c284aaebb879fc9709202284089a7d34dc2e6068
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-24 18:34:40 +0100

    [add] Generic trigger zone

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private TriggerZone agroZone = null;
+
+    [SerializeField]
+    private TriggerZone lostZone = null;
+
+
+    void Start()
+    {
+        agroZone.OnZoneEnter += OnGotTarget;
+        lostZone.OnZoneExit += OnLostTarget;
+    }
+
+    void OnDestroy() 
+    {
+        agroZone.OnZoneEnter -= OnGotTarget;
+        lostZone.OnZoneExit -= OnLostTarget;
+    }
+
+    void OnGotTarget()
+    {
+        Debug.Log("GOT TARGET");
+    }
+
+    void OnLostTarget()
+    {
+        Debug.Log("LOST TARGET");
+    }
+}
---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public event System.Action OnZoneEnter;
+    public event System.Action OnZoneExit;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (OnZoneEnter != null) {
+            OnZoneEnter();
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (OnZoneExit != null) {
+            OnZoneExit();
+        }
+    }
+}
