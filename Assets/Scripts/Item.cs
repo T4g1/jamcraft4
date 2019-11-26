16993192e90728866c54e4fa982f9d9e0bb1abfe
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-26 21:44:22 +0100

    [enh] Use ScriptableObjects for items

--- Assets/Scripts/Item.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-}
---
+++ Assets/Scripts/Items/WeaponPart.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+[CreateAssetMenu(fileName = "New Part", menuName = "Weapons/New Part")]
+public class WeaponPart : Item
+{
+    public Sprite sprite;
+
+    [Header("Quiver")]
+    public bool isQuiver;
+    public uint magazineSize;
+
+    [Header("Barrel")]
+    public bool isBarrel;
+    public Bullet bulletPrefab;
+
+    [Header("Stock")]
+    public bool isSstock;
+    public uint recoil;
+
+    [Header("Sight")]
+    public bool isSight;
+    public uint precision;
+
+    [Header("Handle")]
+    public bool isHandle;
+    public uint reloadTime;
+
+    [Header("String")]
+    public bool isString;
+    public float fireRate;  // Time between every shot
+    public float lifespan;  // Time before decaying
+}
