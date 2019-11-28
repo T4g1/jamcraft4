86d45112ab112eab41057105b78325669087673d
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-28 20:20:16 +0100

    [add] Slimes

--- Assets/Prefabs/Enemies/Slime/Wandering.cs
+++ Assets/Prefabs/Enemies/Slime/Wandering.cs
@@ -17,7 +17,7 @@ public class Wandering : StateMachineBehaviour
     override public void OnStateEnter(
         Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        enemy = animator.gameObject.GetComponent<Enemy>();
+        enemy = animator.gameObject.GetComponentInParent<Enemy>();
         directionChangeDelay = 0.0f;
     }
 
