b252c4cdec965e244f00ad69bb91655e36eac985
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-28 10:08:54 +0100

    [add] Tooltip position

--- Assets/Scripts/GUI/CraftingStation.cs
+++ Assets/Scripts/GUI/CraftingStation.cs
@@ -11,6 +11,8 @@ public class CraftingStation : Interactable
 {
     [SerializeField]
     private GameObject actionInfo = null;
+    [SerializeField]
+    private float actionInfoOffset = 0.5f;
 
     private bool isInRange = false;
     
@@ -39,5 +41,33 @@ public class CraftingStation : Interactable
         if (Input.GetButtonDown("Use") && isInRange) {
             GameController.Instance.ToggleCraftingUI();
         }
+
+        if (actionInfo.activeSelf) {
+            UpdateActionInfoPosition();
+        }
+    }
+
+    void UpdateActionInfoPosition()
+    {
+        Canvas canvas = actionInfo.GetComponentInParent<Canvas>();
+        Vector3 offsetPosition = new Vector3(
+            transform.position.x,
+            transform.position.y + actionInfoOffset,
+            transform.position.z
+        );
+        
+        // Calculate *screen* position (note, not a canvas/recttransform position)
+        Vector2 canvasPosition;
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(offsetPosition);
+        
+        // Convert screen position to Canvas / RectTransform space <- leave camera null if Screen Space Overlay
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform) canvas.transform,
+            screenPoint,
+            null,
+            out canvasPosition
+        );
+        
+        actionInfo.transform.localPosition = canvasPosition;
     }
 }
