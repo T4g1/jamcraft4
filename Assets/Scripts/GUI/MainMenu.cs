2c14b36a16d93e4dd8591717865f17c1ceabc339
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-29 13:37:41 +0100

    [add] Main menu script

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
