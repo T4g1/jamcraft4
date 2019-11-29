c1a2fc475f4aa3e362689240e97cb1a38bf21d71
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-29 10:30:01 +0100

    [add] Camera follows midpoint between mouse and player

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private Player player;
+
+    [SerializeField]
+    private float margin = 100.0f;
+
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPosition = Input.mousePosition;
+        if (screenPosition.x < margin) {
+            screenPosition.x = margin;
+        }
+        if (screenPosition.y < margin) {
+            screenPosition.y = margin;
+        }
+        if (screenPosition.x > Screen.width - margin) {
+            screenPosition.x = Screen.width - margin;
+        }
+        if (screenPosition.y > Screen.height - margin) {
+            screenPosition.y = Screen.height - margin;
+        }
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        Vector3 delta = player.transform.position - worldPosition;
+
+        Vector3 cameraPosition = player.transform.position - delta / 2;
+        cameraPosition.z = -10.0f;
+
+        transform.position = cameraPosition;
+    }
+}
