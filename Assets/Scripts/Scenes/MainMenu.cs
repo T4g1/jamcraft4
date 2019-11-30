e8d56901a537817d58f27704aa9b01315a36ab11
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-11-30 10:29:08 +0100

    [fix] Sound for main menu

---
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string mainThemeName;
+    private FMOD.Studio.EventInstance mainTheme;
+
+
+    void Start()
+    {
+        Screen.SetResolution(640, 360, false);
+
+        mainTheme = FMODUnity.RuntimeManager.CreateInstance(mainThemeName);
+        mainTheme.setParameterByName("intensity", 0.5f);
+        mainTheme.start();
+    }
+
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnDestroy()
+    {
+        mainTheme.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
