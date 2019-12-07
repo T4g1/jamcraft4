2a545b83665794ddf2d619501b3c92c221beeb93
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-12-07 14:06:57 +0100

    [add] Layout for setting screen

---
+++ Assets/Scripts/GUI/Setting/KeySetting.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class KeySetting : MonoBehaviour
+{
+    [SerializeField]
+    private string action = "Action";
+    [SerializeField]
+    private KeyType type = KeyType.KeyDown;
+    [SerializeField]
+    private int mouseButton = 0;
+    [SerializeField]
+    private KeyCode key = KeyCode.Space;
+    [SerializeField]
+    private Color normalColor = Color.white;
+    [SerializeField]
+    private Color selectedColor = Color.blue;
+
+    private string[] mouseButtons = new string[] {
+        "Left click",
+        "Right click",
+        "Middle click"
+    };
+
+    private bool isSelected = false;
+    private Image image;
+    private Text actionText;
+    private Text buttonText;
+    private Button button;
+
+
+    void Awake()
+    {
+        image = GetComponentInChildren<Image>();
+        actionText = GetComponentInChildren<Text>();
+        button = GetComponentInChildren<Button>();
+        buttonText = button.GetComponentInChildren<Text>();
+
+        actionText.text = action;
+
+        OnDeselect();
+    }
+
+    public void UpdateButtonText()
+    {
+        if (type == KeyType.KeyDown) {
+            buttonText.text = key.ToString();
+        }
+        else {
+            if (mouseButton < mouseButtons.Length) {
+                buttonText.text = mouseButtons[mouseButton];
+            }
+            else {
+                buttonText.text = "Mouse button " + mouseButton.ToString();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!isSelected) {
+            return;
+        }
+
+        Event currentEvent = Event.current;
+        if (currentEvent.type == EventType.KeyDown) {
+            type = KeyType.KeyDown;
+            key = currentEvent.keyCode;
+        }
+        else if (currentEvent.type == EventType.MouseDown) {
+            type = KeyType.MouseDown;
+            mouseButton = currentEvent.button;
+        }
+        else {
+            return;
+        }
+
+        OnDeselect();
+    }
+
+    public void OnClick()
+    {
+        if (isSelected) {
+            return;
+        }
+
+        SettingsController.Instance.SettingSelected(this);
+    }
+
+    public void OnDeselect()
+    {
+        if (image == null) {
+            return;
+        }
+        
+        image.color = normalColor;
+        UpdateButtonText();
+        isSelected = false;
+    }
+
+    public void OnSelect()
+    {
+        image.color = selectedColor;
+        buttonText.text = "Press a key";
+        isSelected = true;
+    }
+}
+
+public enum KeyType
+{
+    KeyDown = EventType.KeyDown,
+    MouseDown = EventType.MouseDown
+}
---
+++ Assets/Scripts/Scenes/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SettingsController : MonoBehaviour
+{
+    #region Singleton
+    public static SettingsController Instance { get; private set; }
+
+    void InitInstance()
+    {
+        if (Instance != null) {
+            Debug.Log("More than one SettingsController created!");
+        }
+
+        Instance = this;
+    }
+
+    private void Awake() 
+    {
+        InitInstance();
+    }
+    #endregion
+
+    [SerializeField]
+    private GameObject keySettingContainer = null;
+    private KeySetting[] keySettings;
+
+
+    void Start()
+    {
+        Assert.IsNotNull(keySettingContainer);
+
+        keySettings = 
+            keySettingContainer.GetComponentsInChildren<KeySetting>(true);
+    }
+
+    public void SettingSelected(KeySetting setting)
+    {
+        foreach(KeySetting keySetting in keySettings) {
+            keySetting.OnDeselect();
+        }
+
+        setting.OnSelect();
+    }
+}
