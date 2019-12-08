441b08c16bb679d293ccaae1d46147cc8c6e0748
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-12-08 20:49:45 +0100

    [fix] Firepits triggered too much

---
+++ Assets/Scripts/Lightable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lightObject = null;
+    [SerializeField]
+    private bool triggerOnce = true;
+    private bool triggered = false;
+
+
+    public void OnTriggered()
+    {
+        if (triggerOnce && triggered) {
+            return;
+        }
+        
+        triggered = true;
+
+        if (lightObject == null) {
+            return;
+        }
+        
+        GameController.Instance.Instantiate(
+            lightObject,
+            gameObject.transform.position
+        );
+    }
+}
